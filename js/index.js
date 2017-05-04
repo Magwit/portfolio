1120e8c038e41472428b64be51cf3a37ca897c6d
Magwit <magnushakon@hotmail.com>
2017-05-04 11:01:06 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* If I use any plugins such as popover remember to activate boostrap in the JS file as well*/
+
+// $("i").hover(function(){
+//     $(this).css("color", "ghostwhite");
+//     }, function(){
+//     $(this).css("color", "gainsboro");
+// });
